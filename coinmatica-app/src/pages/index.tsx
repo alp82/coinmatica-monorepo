e376db776af435bb714ef03efcb14e4faa66891b
Alper Ortac <alper@spirable.com>
2022-08-26 23:38:06 +0200

    remove obsolete trpc calls

--- coinmatica-app/src/pages/index.tsx
+++ coinmatica-app/src/pages/index.tsx
@@ -1,12 +1,9 @@
-import type { NextPage } from "next"
-import Head from "next/head"
-import { signIn, signOut, useSession } from "next-auth/react"
-import { trpc } from "../utils/trpc"
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import { signIn, signOut, useSession } from 'next-auth/react'
 import MainLayout from '../components/MainLayout'
 
 const Home: NextPage = () => {
-  const hello = trpc.proxy.example.hello.useQuery({ text: "from tRPC" })
-
   return (
     <>
       <Head>
@@ -18,9 +15,6 @@ const Home: NextPage = () => {
         <h1 className="text-5xl md:text-[5rem] leading-normal font-extrabold text-gray-700">
           Coinmatica <span className="text-purple-300">T3</span> App
         </h1>
-        <div className="flex items-center justify-center w-full pt-6 text-2xl text-blue-500">
-          {hello.data ? <p>{hello.data.greeting}</p> : <p>Loading..</p>}
-        </div>
         <AuthShowcase />
       </MainLayout>
     </>
@@ -31,13 +25,11 @@ export default Home
 
 // Component to showcase protected routes using Auth
 const AuthShowcase: React.FC = () => {
-  const secretMessage = trpc.proxy.auth.getSecretMessage.useQuery()
   const { data: session } = useSession()
 
   return (
     <div>
       {session && <p>Logged in as {session?.user?.name}</p>}
-      {secretMessage.data && <p>{secretMessage.data}</p>}
       <button
         className="px-4 py-2 border-2 border-blue-500 rounded-md"
         onClick={session ? () => signOut() : () => signIn()}
